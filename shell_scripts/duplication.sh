5257cb97df3eb82fa29ccfb9833bc34efd1fef48
peterthorpe5 <pjt6@st-andrews.ac.uk>
2019-06-14 15:56:08 +0100

    duplication commands

--- shell_scripts/duplication.sh
+++ shell_scripts/duplication.sh
@@ -14,3 +14,12 @@ diamond blastp -p 12 --more-sensitive -e 1e-5 -v -q Hs.aa.fasta  -d Hs.dmnd --ou
 # mcscanx
 
 ~/shelf_apps/apps/MCScanX/duplicate_gene_classifier  /shelf/apps/pjt6/Hsh/final/duplication/hs_hs/hs_hs
+
+# collinear_tandem_arrays
+
+~/shelf_apps/apps/MCScanX/downstream_analyses/detect_collinear_tandem_arrays -g hs_hs.gff -b hs_hs.blast -c hs_hs.collinearity -o hs_newton_detect_collinear_tandem_array 
+
+~/shelf_apps/apps/MCScanX/downstream_analyses/detect_collinear_tandem_arrays -g hs_hs.gff -b hs_hs.blast -c hs_hs.collinearity -o hs_detect_collinear_tandem_arrays.out
+
+# 
+perl ~/shelf_apps/apps/MCScanX/downstream_analyses/add_ka_and_ks_to_collinearity.pl -i hs_hs.collinearity -d ../H_sch_gene_calls_v1.codingseq.t1_only.fasta -o hs.collinearity.kaks
